cd8c39b34bced0fd9c7278918c1f0dc6de539f41
tkhs <spain3405@gmail.com>
2021-12-02 21:49:26 +0900

    fix controller & model

--- backend/app/Http/Controllers/ComponentController.php
+++ backend/app/Http/Controllers/ComponentController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Component;
-use Illuminate\Support\Facades\DB;
 
 class ComponentController extends Controller
 {
@@ -12,7 +11,6 @@ class ComponentController extends Controller
     {
         $componet = new Component;
         $list = $componet->getComponentEffectList();
-
         return view('component.index', compact('list'));
     }
 
@@ -23,46 +21,19 @@ class ComponentController extends Controller
         if ($request->file('csv')->getMimeType() !== "application/csv") {
             return redirect()->route("component.index")->withErrors('file type missmatch.');
         }
-
-        // テーブルを初期化
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        DB::table('component_effect')->truncate();
-        DB::table('components')->truncate();
-        DB::table('effects')->truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        
+        $componet = new Component;
+        $componet->trancateRelationTable();
 
         $file_path = $request->file('csv')->path();
         $file = new \SplFileObject($file_path);
         $file->setFlags(\SplFileObject::READ_CSV);
-        $effects = [];
+        
         foreach($file as $index => $line) {
             if ($index === 0) continue;
-
-            $componet = new Component;
-            $componet->name = $line[0];
-            $componet->save();
-
-            for ($i = 1; $i < count($line); $i++) {
-                if ($line[$i] && !in_array($line[$i], $effects, true)) {
-                    $effects[] = $line[$i];
-                    DB::table('effects')->insert([
-                        'name' => $line[$i]
-                    ]);
-                }
-                
-                if ($line[$i]) {
-                    $effect_id = array_search($line[$i], $effects, true) + 1;
-                    DB::table('component_effect')->insert([
-                        'component_id' => $componet->id,
-                        'effect_id' => $effect_id
-                    ]);
-                }
-            }            
+            $componet->saveComponentEffect($line);
         }
 
-        $componet = new Component;
-        $list = $componet->getComponentEffectList();
-
-        return redirect(route('component.index', compact('list')));
+        return redirect()->route('component.index');
     }
 }
