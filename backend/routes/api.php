f4f8b8a772ca0ce59b8d5ad8399435e0396ecd38
tkhs <spain3405@gmail.com>
2021-12-05 20:31:31 +0900

    make api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/ranking', \App\Http\Controllers\RankingProducts::class);
+Route::get('/ranking', \App\Http\Controllers\Api\RankingProducts::class);
+Route::get('/genre', [\App\Http\Controllers\Api\GenreController::class, 'index']);
