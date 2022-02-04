84f8044cf8a13de7b4704b34aa4736ceaffd15d1
t.a <aono@dream-link.jp>
2022-02-04 18:26:19 +0900

    make tsconfig and types

--- frontend/src/components/Top.tsx
+++ frontend/src/components/Top.tsx
@@ -6,13 +6,13 @@ import { Center } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
 
 import styles from "../MainStyles.module.css";
-import CosmeImage from '../images/cosme.png'
-import ReadingSvg from '../svgs/undraw_reading.svg';
-import CrackerSvg from '../svgs/cracker.svg';
+import CosmeImage from "../images/cosme.png";
+import ReadingSvg from "../svgs/undraw_reading.svg";
+import CrackerSvg from "../svgs/cracker.svg";
 
 export const Top = () => {
   const url = `${process.env.REACT_APP_BACKEND_HOST}api/count`;
-  const fetcher = (arg) => fetch(arg).then((res) => res.json());
+  const fetcher = (arg: string): Promise<number> => fetch(arg).then((res) => res.json());
   const { data, error } = useSWR(url, fetcher);
 
   if (error) return <Center>データのアクセスに失敗しました。</Center>;
@@ -24,44 +24,44 @@ export const Top = () => {
           お気に入りスキンケアを見つけよう
         </Box>
       </Center>
-      <Box my="20" position="relative" left="50%" transform="translateX(-50%)" w="100vw" >
+      <Box my="20" position="relative" left="50%" transform="translateX(-50%)" w="100vw">
         <Image src={CosmeImage} h="80vw" maxH="800px" objectFit="cover" m="0 auto" />
       </Box>
       {data ? (
         <Center>
-          <Image src={CrackerSvg} w='14' />
+          <Image src={CrackerSvg} w="14" />
           <Box fontSize="xl">登録商品数 {data.toLocaleString()} 点</Box>
-          <Image src={CrackerSvg} w='14' />
+          <Image src={CrackerSvg} w="14" />
         </Center>
-      ) : ''}
+      ) : (
+        ""
+      )}
       <Center>
         <Box>商品随時追加中！</Box>
       </Center>
-      <Box lineHeight="2.5em" mt='6em'>
-        どれが自分に自分にあっているか分からない。<br />
-        商品があり過ぎて探すのが疲れる。<br />
-        悩みを解消できる商品を効率的に見つけたい。<br />
+      <Box lineHeight="2.5em" mt="6em">
+        どれが自分に自分にあっているか分からない。
         <br />
-        そのように考えた事はありませんか？<br />
+        商品があり過ぎて探すのが疲れる。
+        <br />
+        悩みを解消できる商品を効率的に見つけたい。
+        <br />
+        <br />
+        そのように考えた事はありませんか？
+        <br />
         ３つの質問に答えるだけで、あなたの合わせたランキングを紹介します!
       </Box>
-      <Box mt='20' mb='7em'>
+      <Box mt="20" mb="7em">
         <Image src={ReadingSvg} />
       </Box>
       <Center>
         <Link to="/question">
-          <Button
-            className={styles.primaryBtn}
-            colorScheme="red500"
-            height='65px'
-            width='200px'
-            size="lg"
-          >
+          <Button className={styles.primaryBtn} colorScheme="red500" height="65px" width="200px" size="lg">
             やってみる
           </Button>
         </Link>
       </Center>
-      <Box mt="7em" position="relative" left="50%" transform="translateX(-50%)" w="100vw" >
+      <Box mt="7em" position="relative" left="50%" transform="translateX(-50%)" w="100vw">
         <Image src={CosmeImage} h="80vw" maxH="800px" objectFit="cover" m="0 auto" />
       </Box>
     </>
